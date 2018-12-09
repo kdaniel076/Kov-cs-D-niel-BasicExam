8210f51afe99d9303a48628823dd1d87e24364fa
Kovács Dániel <kovacsdaniel076@gmail.com>
2018-12-10 00:16:11 +0100

    modified js file... de minek?

--- js/got.js
+++ js/got.js
@@ -13,6 +13,7 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
+  displayCharacters(userDatas);
 }
 
 getGameOfThronesCharacterDatas(
@@ -22,3 +23,34 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+function displayCharacters(userDatas) {
+  var aliveCharacters = [];
+  for (var i = 0; i < userDatas.length; i++) {
+    if (!userDatas[i].hasOwnProperty('dead')) {
+      aliveCharacters.push(userDatas[i]);
+    }
+  }
+  for (var i = 0; i < aliveCharacters.length - 1; i++) {
+    for (var j = i+1; j < aliveCharacters.length; j++) {
+      if (aliveCharacters[i].name > aliveCharacters[j].name) {
+        var temp = [aliveCharacters[i], aliveCharacters[j]];
+        aliveCharacters[i] = temp[1];
+        aliveCharacters[j] = temp[0];
+      }
+    }
+  }
+
+  function showDetails(i) {
+    showDetails(i)
+    alert(aliveCharacters[i].name);
+  }
+  
+  for (var i = 0; i < aliveCharacters.length; i++) {
+    var div = document.querySelector(".main-div")
+    div.innerHTML += `<div id="i" class="main-div__char-divs">
+                      <img class="main-div__char-pics" src="${aliveCharacters[i].portrait}">
+                      <span onclick="showDetails(${i})" class="main-div__char-spans">${aliveCharacters[i].name}</span>
+                      </div>`
+  }
+}
